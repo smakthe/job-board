c13708004be7f24a5b1c725070169cd131964a86
smakthe <scmakra99@gmail.com>
2025-05-16 16:39:23 +0530

    Updated webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,25 +1,19 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
+// Rails dependencies
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-// Bootstrap and jQuery dependencies
+// Bootstrap dependencies
+import 'jquery'
+import 'popper.js'
 import 'bootstrap'
-import jQuery from 'jquery'
-global.$ = global.jQuery = jQuery
-window.$ = window.jQuery = jQuery
 
-// Import custom styles
-import '../stylesheets/application'
+// Import the stylesheet (adjust path if needed)
+import '../stylesheets/application'  
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true) 
+// Bootstrap jQuery plugins
+document.addEventListener("turbolinks:load", function() {
+  // Bootstrap dropdown initialization (if needed)
+  $('[data-toggle="dropdown"]').dropdown();
+})
